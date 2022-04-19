1eb80da572029492f77bf106f6e1edb2a9fa118b
yang <yrzcode@hotmail.com>
2022-04-19 14:27:59 +0900

    v3
    add lombok

--- src/main/java/com/sample/springlogin/user/User.java
+++ src/main/java/com/sample/springlogin/user/User.java
@@ -1,41 +1,50 @@
 package com.sample.springlogin.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Getter
     private static final long serialVersionUID = 1L;
     private String accountId;
     private String password;
 
-    public User() {};
+//    public User() {};
 
-    public User(String accountId, String password) {
-        this.accountId = accountId;
-        this.password = password;
-    }
+//    public User(String accountId, String password) {
+//        this.ac   countId = accountId;
+//        this.password = password;
+//    }
+//
+//    public String getAccountId() {
+//        return accountId;
+//    }
+//
+//    public void setAccountId(String accountId) {
+//        this.accountId = accountId;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 
-    public String getAccountId() {
-        return accountId;
-    }
+//    public static long getSerialVersionUID() {
+//        return serialVersionUID;
+//    }
 
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public static long gtSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    @Override
-    public String toString() {
-        return "User accountId=" + accountId + ", password=" + password + "]";
-    }
+//    @Override
+//    public String toString() {
+//        return "User accountId=" + accountId + ", password=" + password + "]";
+//    }
 
 }
